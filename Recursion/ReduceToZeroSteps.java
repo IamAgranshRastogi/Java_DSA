c6237208f8e9efbea648cadc11bff38f37186b2f
mr_m <mr_m@gmail.com>
2023-04-12 08:52:25 +0530

    edit

--- Recursion/ReduceToZeroSteps.java
+++ Recursion/ReduceToZeroSteps.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class RecuceToZeroSteps 
+public class ReduceToZeroSteps 
 { public static void main(String[] args) 
   { Scanner s=new Scanner(System.in);
     System.out.println("Enter a number : ");
